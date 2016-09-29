18eb8ea6c3f2b314f6f1af271d6ee425eea8b6d7
andresuhartanto <andresuhartanto28@gmail.com>
2016-09-29 15:07:08 +0800

    Google map added

---
+++ Locus/MarkerInforView.swift
@@ -0,0 +1,15 @@
+//
+//  uiMarkerInforView.swift
+//  Locus
+//
+//  Created by Andre Suhartanto on 9/27/16.
+//  Copyright © 2016 EndeJeje. All rights reserved.
+//
+
+import UIKit
+
+class MarkerInforView: UIView {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
