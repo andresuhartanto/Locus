59863ef2b056f0a40d54435485491a1dd501e537
andresuhartanto <andresuhartanto28@gmail.com>
2016-09-30 14:38:42 +0800

    remove pods

--- Pods/Target Support Files/Pods-Locus/Pods-Locus-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Locus : NSObject
-@end
-@implementation PodsDummy_Pods_Locus
-@end
--- Pods/Target Support Files/Pods-Locus/Pods-Locus-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_LocusVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_LocusVersionString[];
-
